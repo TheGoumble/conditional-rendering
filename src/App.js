5fbe567e8fe3f0fabc880b8c64317da90398ee72
Javier Vargas <3231javiervargas@gmail.com>
2022-08-08 11:50:19 -0400

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import Loading from "./components/Loading"
+import Loaded from "./components/Loaded"
+import "./App.css"
 
 function App() {
+  const [isloaded, setIsLoaded] = useState(false)
+  const [objectExample, setObjectExample] = useState({ name: "name of object" })
+  const [username, setUsername] = useState("")
+  const numbers = [1,2,3,4,5,6,7,8,9,0]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Username is: {username !== "" ? username : "Guest"}</h1>
+      <h1>Username is: {username || "Guest"}</h1>
+      <h1>{
+        isloaded &&
+        <>
+          {numbers.map((num)=>{
+            return num + " & "
+          })}
+          <Loaded />
+          <h2>I have {numbers.length} numbers</h2>
+        </>}
+      </h1>
+      <button
+        onClick={() => {
+          console.log("button clicked")
+          setIsLoaded(!isloaded)
+        }}
+      >
+        Change Loaded
+      </button>
+
+      <h1>{objectExample.name}</h1>
+      <h1>{isloaded ? "Yes" : "No"}</h1>
+
+      {isloaded ? <Loaded /> : <Loading />}
+
+      <h1>isloaded = {String(isloaded)}</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Loaded.jsx
@@ -0,0 +1,9 @@
+function Loaded() {
+  return (
+    <>
+      <h2>It's now loaded</h2>
+    </>
+  )
+}
+
+export default Loaded
